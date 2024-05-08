7b692161798c556db30b53989d3c20ba1f96a969
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-08 18:29:52 +0300

    routes/redux provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,26 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./scss/styles.scss";
 import "bootstrap";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { LoginContainer } from "./features/Login/containers/LoginContainer.tsx";
+import { Provider } from "react-redux";
+import { store } from "./store/store.ts";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+    {
+        path: "/login",
+        element: <LoginContainer />,
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <RouterProvider router={router} />
+        </Provider>
     </React.StrictMode>
 );
