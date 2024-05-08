f56282253744f6e3a42d659e03611e103425d69b
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-08 18:32:40 +0300

    use form hook

---
+++ src/features/common/hooks/useForm.ts
@@ -0,0 +1,26 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+
+export const useForm = <T>(initialValue: T, onSubmitHandler?: (values: T) => void) => {
+    const [formValues, setFormValues] = useState(initialValue);
+
+    const onChangeHandler = (e: FormEvent | ChangeEvent) => {
+        const element = e.target as HTMLInputElement;
+        setFormValues((state) => ({ ...state, [element.name]: element.value }));
+    };
+
+    const onSubmit = (e: FormEvent) => {
+        e.preventDefault();
+        if (onSubmitHandler) onSubmitHandler(formValues);
+    };
+
+    const resetForm = () => {
+        setFormValues(initialValue);
+    };
+
+    return {
+        formValues,
+        onSubmit,
+        onChangeHandler,
+        resetForm,
+    };
+};
