02342dc35ad63389fe6930af6eee11d69e401498
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-08 19:55:48 +0300

    register container & register interface

---
+++ src/features/Register/containers/RegisterContainer.tsx
@@ -0,0 +1,12 @@
+import Container from "../../common/components/Container/Container";
+import RegisterForm from "../components/RegisterForm/RegisterForm";
+
+const RegisterContainer = () => {
+    return (
+        <Container>
+            <RegisterForm />
+        </Container>
+    );
+};
+
+export default RegisterContainer;
