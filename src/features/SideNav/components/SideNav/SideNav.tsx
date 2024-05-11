1db003219e30ed6567f47008279307720cac118f
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-11 15:04:43 +0300

    Side nav adjustments

--- src/features/SideNav/components/SideNav/SideNav.tsx
+++ src/features/SideNav/components/SideNav/SideNav.tsx
@@ -1,15 +1,26 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Offcanvas from "react-bootstrap/Offcanvas";
 import ButtonC from "../../../common/components/ButtonC/ButtonC";
 import styles from "./SideNav.module.scss";
 import { Link } from "react-router-dom";
 import { Nav } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import { IRootState } from "../../../../store/store";
 
 export const SideNav = () => {
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+
+    const linkRefs = useRef<HTMLAnchorElement[]>([]);
+
+    const handleTabClick = (tabIndex: number) => {
+        handleClose();
+        linkRefs.current[tabIndex].click();
+    };
+
+    const user = useSelector((state: IRootState) => state.user);
 
     return (
         <>
@@ -18,13 +29,47 @@ export const SideNav = () => {
             </ButtonC>
             <Offcanvas show={show} onHide={handleClose} placement="end">
                 <Offcanvas.Header closeButton>
-                    <Offcanvas.Title>Menu</Offcanvas.Title>
+                    <Offcanvas.Title>Menu {user.firstName ? `- ${user.firstName} ${user.lastName}` : ''}</Offcanvas.Title>
                 </Offcanvas.Header>
                 <Offcanvas.Body>
-                    <Nav className="flex-column">
-                        <Link to="/" onClick={handleClose}>Home</Link>
-                        <Link to="/register" onClick={handleClose}>Register</Link>
-                        <Link to="/login" onClick={handleClose}>Login</Link>
+                    <Nav className={`flex-column ${styles["nav"]}`} variant="pills">
+                        <Nav.Item onClick={() => handleTabClick(0)}>
+                            <Nav.Link eventKey="first">
+                                <Link to="/" ref={(el) => (el != null ? (linkRefs.current[0] = el) : {})}>
+                                    Home
+                                </Link>
+                            </Nav.Link>
+                        </Nav.Item>
+
+                        {!user.email && (
+                            <>
+                                <Nav.Item onClick={() => handleTabClick(1)}>
+                                    <Nav.Link eventKey="second">
+                                        <Link ref={(el) => (el != null ? (linkRefs.current[1] = el) : {})} to="/register">
+                                            Register
+                                        </Link>
+                                    </Nav.Link>
+                                </Nav.Item>
+
+                                <Nav.Item onClick={() => handleTabClick(2)}>
+                                    <Nav.Link eventKey="third">
+                                        <Link ref={(el) => (el != null ? (linkRefs.current[2] = el) : {})} to="/login">
+                                            Login
+                                        </Link>
+                                    </Nav.Link>
+                                </Nav.Item>
+                            </>
+                        )}
+
+                        {user.email && 
+                            <Nav.Item onClick={() => handleTabClick(3)}>
+                            <Nav.Link eventKey="third">
+                                <Link ref={(el) => (el != null ? (linkRefs.current[3] = el) : {})} to="/logout">
+                                    Logout
+                                </Link>
+                            </Nav.Link>
+                        </Nav.Item>
+                        }
                     </Nav>
                 </Offcanvas.Body>
             </Offcanvas>
