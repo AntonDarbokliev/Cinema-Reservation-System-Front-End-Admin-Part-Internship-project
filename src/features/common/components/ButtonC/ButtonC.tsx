fe82be1840f79930fc3a995300118bff37b249eb
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-11 13:57:00 +0300

    adjust custom button

--- src/features/common/components/ButtonC/ButtonC.tsx
+++ src/features/common/components/ButtonC/ButtonC.tsx
@@ -1,11 +1,15 @@
 import Button  from "react-bootstrap/Button";
+import styles from './ButtonC.module.scss';
+import { ReactNode } from "react";
 
 interface Props {
-    text: string;
+    children: ReactNode;
+    onClick?: () => void;
+    additionalClasses?: string
 }
 
-const ButtonC: React.FC<Props> = (props) => {
-    return <Button type="submit">{props.text}</Button>;
+const ButtonC: React.FC<Props> = ({children, onClick, additionalClasses}) => {
+    return <Button type="submit" onClick={onClick} className={styles['custom-button'] + ` ${additionalClasses}`}>{children}</Button>;
 };
 
 export default ButtonC;
