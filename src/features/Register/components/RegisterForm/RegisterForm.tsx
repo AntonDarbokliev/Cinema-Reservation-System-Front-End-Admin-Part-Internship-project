7fac59c03f161104fef3c29befe27c123250560b
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-11 13:57:43 +0300

    Register form adjustment

--- src/features/Register/components/RegisterForm/RegisterForm.tsx
+++ src/features/Register/components/RegisterForm/RegisterForm.tsx
@@ -6,7 +6,7 @@ import { useRegister } from "../../hooks/useRegister";
 const RegisterForm = () => {
     const { registerHandler } = useRegister();
 
-    const { formValues, onChangeHandler, onSubmit } = useForm(
+    const { formValues, onChangeHandler, onSubmit, validated } = useForm(
         {
             firstName: "",
             lastName: "",
@@ -17,27 +17,31 @@ const RegisterForm = () => {
     );
 
     return (
-        <Form onSubmit={onSubmit}>
+        <Form onSubmit={onSubmit} noValidate validated={validated}>
             <Form.Group className="mb-3" controlId="formBasicFirstName">
                 <Form.Label>First Name</Form.Label>
-                <Form.Control name="firstName" value={formValues.firstName} onChange={onChangeHandler} />
+                <Form.Control required name="firstName" minLength={2} value={formValues.firstName} onChange={onChangeHandler} />
+                <Form.Control.Feedback type="invalid">Please provide a name.</Form.Control.Feedback>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicLastName">
                 <Form.Label>Last Name</Form.Label>
-                <Form.Control name="lastName" value={formValues.lastName} onChange={onChangeHandler} />
+                <Form.Control required name="lastName" value={formValues.lastName} onChange={onChangeHandler} />
+                <Form.Control.Feedback type="invalid">Please provide a name.</Form.Control.Feedback>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control name="email" value={formValues.email} onChange={onChangeHandler} type="email" placeholder="e.g. bob@cinema.com" />
+                <Form.Control required name="email" value={formValues.email} onChange={onChangeHandler} type="email" placeholder="e.g. bob@cinema.com" />
+                <Form.Control.Feedback type="invalid">Please provide a valid email.</Form.Control.Feedback>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control name="password" value={formValues.password} onChange={onChangeHandler} type="password" placeholder="Password" />
+                <Form.Control required name="password" value={formValues.password} onChange={onChangeHandler} type="password" placeholder="Password" />
+                <Form.Control.Feedback type="invalid">Please provide a password (5 chars min).</Form.Control.Feedback>
             </Form.Group>
-            <ButtonC text="Register" />
+            <ButtonC>Register</ButtonC>
         </Form>
     );
 };
