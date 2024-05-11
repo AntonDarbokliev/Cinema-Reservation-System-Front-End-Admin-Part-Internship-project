a4e2033c2be8c390bab0a9f11bd03d15f30d0807
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-11 13:58:28 +0300

    Routing

--- src/main.tsx
+++ src/main.tsx
@@ -8,25 +8,34 @@ import { LoginContainer } from "./features/Login/containers/LoginContainer.tsx";
 import { Provider } from "react-redux";
 import { store } from "./store/store.ts";
 import RegisterContainer from "./features/Register/containers/RegisterContainer.tsx";
+// import { SideNav } from "./features/SideNav/components/SideNav/SideNav.tsx";
+import { RootOverlay } from "./features/common/components/RootOverlay/RootOverlay.tsx";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <App />,
-    },
-    {
-        path: "/login",
-        element: <LoginContainer />,
-    },
-    {
-        path: "/register",
-        element: <RegisterContainer />,
+        element: <RootOverlay />,
+        children: [
+            {
+                path: "/",
+                element: <App />,
+            },
+            {
+                path: "/login",
+                element: <LoginContainer />,
+            },
+            {
+                path: "/register",
+                element: <RegisterContainer />,
+            },
+        ],
     },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <Provider store={store}>
+            {/* <SideNav /> */}
             <RouterProvider router={router} />
         </Provider>
     </React.StrictMode>
