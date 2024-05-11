2c8cdb6845dda1b307f1a5818869a85d4521b4e4
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-11 13:57:34 +0300

    Login form adjustment

--- src/features/Login/components/LoginForm/LoginForm.tsx
+++ src/features/Login/components/LoginForm/LoginForm.tsx
@@ -1,32 +1,42 @@
-import { Form } from "react-bootstrap";
 import { useLogin } from "../../hooks/useLogin";
 import { useForm } from "../../../common/hooks/useForm";
-import ButtonC from '../../../common/components/ButtonC/ButtonC';
-import './LoginForm.scss';
+import ButtonC from "../../../common/components/ButtonC/ButtonC";
+import "./LoginForm.scss";
+import Form from "react-bootstrap/Form";
 
 export const LoginForm = () => {
-  const { loginHandler } = useLogin();
+    const { loginHandler } = useLogin();
 
-  const { formValues, onChangeHandler, onSubmit } = useForm(
-      {
-          email: "",
-          password: "",
-      },
-      async (): Promise<void> => await loginHandler(formValues)
-  );
+    const { formValues, onChangeHandler, onSubmit, validated } = useForm(
+        {
+            email: "",
+            password: "",
+        },
+        async (): Promise<void> => await loginHandler(formValues)
+    );
 
     return (
-        <Form onSubmit={onSubmit}>
+        <Form onSubmit={onSubmit} noValidate validated={validated}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control name="email" value={formValues.email} onChange={onChangeHandler} type="email" placeholder="Enter email" />
+                <Form.Control required name="email" value={formValues.email} onChange={onChangeHandler} type="email" placeholder="Enter email" />
+                <Form.Control.Feedback type="invalid">Please provide a valid email.</Form.Control.Feedback>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control name="password" value={formValues.password} onChange={onChangeHandler} type="password" placeholder="Password" />
+                <Form.Control
+                    required
+                    minLength={5}
+                    name="password"
+                    value={formValues.password}
+                    onChange={onChangeHandler}
+                    type="password"
+                    placeholder="Password"
+                />
+                <Form.Control.Feedback type="invalid">Please provide a password (5 chars min).</Form.Control.Feedback>
             </Form.Group>
-            <ButtonC text="Login"/>
+            <ButtonC>Login</ButtonC>
         </Form>
     );
 };
