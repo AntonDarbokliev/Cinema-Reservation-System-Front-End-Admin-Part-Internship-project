6f7058500ca298d41fca620dc602b3e2135c0d62
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-12 16:55:26 +0300

    cinemas list

---
+++ src/features/CinemasList/components/CinemasList/CinemasList.tsx
@@ -0,0 +1,15 @@
+import ListGroup from "react-bootstrap/ListGroup";
+import { CinemaCard } from "../CinemaCard/CinemaCard";
+import { Cinema } from "../../interfaces/cinemaInterface";
+
+interface Props {
+  cinemas: Cinema[]
+}
+
+export const CinemasList: React.FC<Props> = ({cinemas}) => {
+    return (
+        <ListGroup draggable={true}>
+            {cinemas.map(c => <CinemaCard key={c._id} cinema={c}/>)}
+        </ListGroup>
+    );
+};
