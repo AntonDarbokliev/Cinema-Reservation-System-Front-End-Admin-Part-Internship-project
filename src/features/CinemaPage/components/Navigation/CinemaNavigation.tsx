e59513646b9ce9b640b3ec633cd6be7c8d25013b
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-12 16:55:15 +0300

    Cinema page navigation

---
+++ src/features/CinemaPage/components/Navigation/CinemaNavigation.tsx
@@ -0,0 +1,30 @@
+import { Nav } from "react-bootstrap";
+import styles from "./CinemaNavigation.module.scss";
+import { Link } from "react-router-dom";
+
+export const CinemaNavigation = () => {
+    return (
+        <Nav justify variant="tabs" defaultActiveKey="/home" className={styles["nav"]}>
+            <Nav.Item className={styles["nav-item"]}>
+                <Nav.Link className={styles["nav-link"]} eventKey="link-0">
+                    <Link to="/">Info</Link>
+                </Nav.Link>
+            </Nav.Item>
+            <Nav.Item className={styles["nav-item"]}>
+                <Nav.Link className={styles["nav-link"]} eventKey="link-1">
+                    <Link to="/">Halls</Link>
+                </Nav.Link>
+            </Nav.Item>
+            <Nav.Item className={styles["nav-item"]}>
+                <Nav.Link className={styles["nav-link"]} eventKey="link-2">
+                    <Link to="/">Projections</Link>
+                </Nav.Link>
+            </Nav.Item>
+            <Nav.Item className={styles["nav-item"]}>
+                <Nav.Link className={styles["nav-link"]} eventKey="link-3">
+                    <Link to="/">Menu</Link>
+                </Nav.Link>
+            </Nav.Item>
+        </Nav>
+    );
+};
