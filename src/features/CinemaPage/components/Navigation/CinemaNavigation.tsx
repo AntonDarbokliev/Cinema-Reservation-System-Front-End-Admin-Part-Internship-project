fa018258820a398e9fd8708bc80f5a7d0ecf7b7f
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-14 13:19:17 +0300

    tweak cinema navigation and routing

--- src/features/CinemaPage/components/Navigation/CinemaNavigation.tsx
+++ src/features/CinemaPage/components/Navigation/CinemaNavigation.tsx
@@ -6,22 +6,22 @@ export const CinemaNavigation = () => {
     return (
         <Nav justify variant="tabs" defaultActiveKey="/home" className={styles["nav"]}>
             <Nav.Item className={styles["nav-item"]}>
-                <Nav.Link className={styles["nav-link"]} eventKey="link-0">
+                <Nav.Link as={'div'} className={styles["nav-link"]} eventKey="link-0">
                     <Link to="/">Info</Link>
                 </Nav.Link>
             </Nav.Item>
             <Nav.Item className={styles["nav-item"]}>
-                <Nav.Link className={styles["nav-link"]} eventKey="link-1">
-                    <Link to="/">Halls</Link>
+                <Nav.Link as={'div'}  className={styles["nav-link"]} eventKey="link-1">
+                    <Link to="halls">Halls</Link>
                 </Nav.Link>
             </Nav.Item>
             <Nav.Item className={styles["nav-item"]}>
-                <Nav.Link className={styles["nav-link"]} eventKey="link-2">
+                <Nav.Link as={'div'}  className={styles["nav-link"]} eventKey="link-2">
                     <Link to="/">Projections</Link>
                 </Nav.Link>
             </Nav.Item>
             <Nav.Item className={styles["nav-item"]}>
-                <Nav.Link className={styles["nav-link"]} eventKey="link-3">
+                <Nav.Link as={'div'}  className={styles["nav-link"]} eventKey="link-3">
                     <Link to="/">Menu</Link>
                 </Nav.Link>
             </Nav.Item>
