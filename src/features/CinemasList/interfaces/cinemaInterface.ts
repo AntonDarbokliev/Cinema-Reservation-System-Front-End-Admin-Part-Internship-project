6d4f61aeaa0d984c60867b955bfe3c99bf90c928
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-14 13:17:46 +0300

    cinema interface

---
+++ src/features/CinemasList/interfaces/cinemaInterface.ts
@@ -0,0 +1,15 @@
+export interface Cinema {
+    _id: string;
+
+    address: string;
+
+    name: string;
+
+    numberOfHalls: string;
+
+    hallPlans: object[];
+
+    menu: object[];
+
+    projections: object[];
+}
