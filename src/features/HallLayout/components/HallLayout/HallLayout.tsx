69de2f6a6bb8b5d3580e0b06cfea8e1d314f580e
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-15 15:25:29 +0300

    Add/remove seats

--- src/features/HallLayout/components/HallLayout/HallLayout.tsx
+++ src/features/HallLayout/components/HallLayout/HallLayout.tsx
@@ -1,46 +1,86 @@
 import { Col, Container, Row } from "react-bootstrap";
 import "./HallLayout.scss";
 import { useHall } from "../../hooks/useHall";
-// import chair from "../../../../assets/chair-solid-normal.svg";
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faChair} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChair } from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from "react";
+import ButtonC from "../../../common/components/ButtonC/ButtonC";
+import { Row as RowType, Seat, SeatType } from "../../../HallsList/interfaces/hallInterface";
 export const HallLayout = () => {
     const { hall } = useHall();
+    const [editMode, setEditMode] = useState<boolean>(false);
+    const [addSeatType, setAddSeatType] = useState<SeatType>(SeatType.SEAT_COMMON);
+    const [rows, setRows] = useState<RowType[]>([]);
     let currentSeatNumber = 0;
+    // const rowsCopy = [...hall.seatsLayout];
+    useEffect(() => {
+        setRows([...hall.seatsLayout]);
+    }, [hall.seatsLayout]);
+
+    const seatOnClickHandler = (seat: Seat, rowIndex: number) => {
+        if (editMode) {
+            console.log(seat.type);
+            if (seat.type == SeatType.SEAT_BLANK) {
+                seat.type = addSeatType;
+                // console.log("Clicked on blank seat");
+                //TODO: Make it so clicking on a seat removes the seat or adds it on the place where the user has clicked
+                // depending on the seat type selected (can be selected when edit mode is enabled)
+                // also, instead of having a literal blank, add something so the user knows where he can place seats when in edit mode
+                // also, add an add/remove row feature
+            } else {
+                seat.type = SeatType.SEAT_BLANK;
+            }
+            const rowsCopy = [...rows];
+            const seatsCopy = rowsCopy[rowIndex].seats;
+            const seatIndex = seatsCopy.findIndex((currentSeat) => currentSeat._id == seat._id);
+            seatsCopy[seatIndex] = seat;
+            rowsCopy[rowIndex].seats = seatsCopy;
+            setRows(rowsCopy);
+        }
+    };
+
     return (
         <>
-            <h1>Screen</h1>
-            <Container fluid>
-                {hall.seatsLayout.map((row) => {
+            <h1 style={{ textAlign: "center", marginBottom: "3rem" }}>Screen</h1>
+            <Container>
+                {rows.map((row, rowIndex) => {
                     currentSeatNumber = 0;
                     return (
-                        <Row>
-                            {row.map((seat) => {
-                                if (seat != "blank") {
+                        <Row key={row._id}>
+                            {row.seats.map((seat) => {
+                                if (seat.type != SeatType.SEAT_BLANK) {
                                     currentSeatNumber++;
+
                                     return (
-                                        <Col className="col-sm">
-                                            <FontAwesomeIcon className="chair-icon" icon={faChair}/>
-                                            {/* <svg viewBox="0 0 448 512">
-                                                <path
-                                                    fill="#29d13d"
-                                                    d="M248 48V256h48V58.7c23.9 13.8 40 39.7 40 69.3V256h48V128C384 57.3 326.7 0 256 0H192C121.3 0 64 57.3 64 128V256h48V128c0-29.6 16.1-55.5 40-69.3V256h48V48h48zM48 288c-12.1 0-23.2 6.8-28.6 17.7l-16 32c-5 9.9-4.4 21.7 1.4 31.1S20.9 384 32 384l0 96c0 17.7 14.3 32 32 32s32-14.3 32-32V384H352v96c0 17.7 14.3 32 32 32s32-14.3 32-32V384c11.1 0 21.4-5.7 27.2-15.2s6.4-21.2 1.4-31.1l-16-32C423.2 294.8 412.1 288 400 288H48z"
-                                                    />
-                                            </svg> */}
-                                                    <h4>
-                                                       {currentSeatNumber}
-                                                   </h4>
-                                                   <p>{seat}</p>
+                                        <Col key={seat._id} className="col-sm" onClick={() => seatOnClickHandler(seat, rowIndex)}>
+                                            <FontAwesomeIcon className="chair-icon" icon={faChair} />
+                                            {!editMode && (
+                                                <>
+                                                    <p>
+                                                        {currentSeatNumber} - {seat.type}
+                                                    </p>
+                                                </>
+                                            )}
                                         </Col>
                                     );
                                 } else {
-                                    return <Col style={{ border: "none" }} />;
+                                    if (editMode) {
+                                        return (
+                                            <Col
+                                                onClick={() => seatOnClickHandler(seat, rowIndex)}
+                                                key={seat._id}
+                                                style={{ border: "none", background: "#cccc" }}
+                                            />
+                                        );
+                                    }
+                                    return <Col key={seat._id} style={{ border: "none" }} />;
                                 }
                             })}
                         </Row>
                     );
                 })}
             </Container>
+            <ButtonC onClick={() => setEditMode(!editMode)}>{editMode ? "Save" : "Edit Mode"}</ButtonC>
         </>
     );
 };
