13cac534c985d65b7d41dea3d387f020a67b6fcc
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-15 17:04:51 +0300

    edit hall service

--- src/features/HallLayout/service/hallLayoutService.ts
+++ src/features/HallLayout/service/hallLayoutService.ts
@@ -1,7 +1,8 @@
-import { Hall } from "../../HallsList/interfaces/hallInterface";
+import { CreateRow, Hall } from "../../HallsList/interfaces/hallInterface";
 import { RequestFactory } from "../../common/services/requester";
 
 const request = RequestFactory();
 const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
 export const getHall = (id: string): Promise<Hall> => request.get(`${baseUrl}/halls/${id}`);
+export const editHall = (id: string, layout: { seatsLayout: CreateRow[] }): Promise<Hall> => request.patch(`${baseUrl}/halls/${id}`, layout);
