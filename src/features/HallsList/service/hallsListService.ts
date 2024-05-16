6020a2b0d872033f9fbe96df6a3f40e074ffc825
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-16 17:17:15 +0300

    get halls for the cinema (instead of all halls) from the db

--- src/features/HallsList/service/hallsListService.ts
+++ src/features/HallsList/service/hallsListService.ts
@@ -1,8 +1,12 @@
-import { RequestFactory } from "../../common/services/requester"
+import { Cinema } from "../../CinemasList/interfaces/cinemaInterface";
+import { RequestFactory } from "../../common/services/requester";
 import { Hall } from "../interfaces/hallInterface";
 
 const request = RequestFactory();
 const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
-export const getHalls = (): Promise<Hall[]> => request.get(`${baseUrl}/halls`);
+export const getHalls = async (cinemaId: string): Promise<Hall[]> => {
 
+  const cinema: Cinema = await request.get(`${baseUrl}/cinemas/${cinemaId}`);
+  return cinema.halls;
+};
