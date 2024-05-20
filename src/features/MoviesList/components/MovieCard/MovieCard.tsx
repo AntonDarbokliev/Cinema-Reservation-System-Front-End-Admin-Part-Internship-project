96ba18d47e8fc6954511d5f1155ddcfa61bd4d5a
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-20 21:40:55 +0300

    Movie card

---
+++ src/features/MoviesList/components/MovieCard/MovieCard.tsx
@@ -0,0 +1,24 @@
+// import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import { Movie } from "../../interfaces/Movie";
+import Button from "../../../common/components/Button/Button";
+import { useNavigate } from "react-router-dom";
+import styles from './MovieCard.module.scss';
+
+interface Props {
+    movie: Movie;
+}
+
+export const MovieCard: React.FC<Props> = ({ movie }) => {
+    const navigate = useNavigate();
+    return (
+        <Card className={styles["movie-card"]} >
+            <Card.Img variant="top" src={movie.poster} />
+            <Card.Body>
+                <Card.Title>{movie.name}</Card.Title>
+                <Card.Text>{movie.genres}</Card.Text>
+                <Button onClick={() => navigate(movie._id)}>Go to</Button>
+            </Card.Body>
+        </Card>
+    );
+};
