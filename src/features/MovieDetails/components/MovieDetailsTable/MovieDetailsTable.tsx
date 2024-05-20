5ea7b2498f2996e2e264fe99e730262776b2426a
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-20 21:41:35 +0300

    movie details table

---
+++ src/features/MovieDetails/components/MovieDetailsTable/MovieDetailsTable.tsx
@@ -0,0 +1,43 @@
+import { Table } from "react-bootstrap";
+import { Movie } from "../../../MoviesList/interfaces/Movie";
+
+interface Props {
+    movie: Movie;
+}
+
+export const MovieDetailsTable: React.FC<Props> = ({ movie }) => {
+    return (
+        <Table striped>
+            <tbody>
+                <tr>
+                    <td>Movie Name</td>
+                    <td>{movie.name}</td>
+                </tr>
+                <tr>
+                    <td>Director</td>
+                    <td>{movie.director}</td>
+                </tr>
+                <tr>
+                    <td>Genre/s</td>
+                    <td>{movie.genres}</td>
+                </tr>
+                <tr>
+                    <td>Actors</td>
+                    <td>{movie.actors}</td>
+                </tr>
+                <tr>
+                    <td>Age Rating</td>
+                    <td>{movie.rating}</td>
+                </tr>
+                <tr>
+                    <td>Language</td>
+                    <td>{movie.language}</td>
+                </tr>
+                <tr>
+                    <td>Subtitles</td>
+                    <td>{movie.subtitles ?? 'No subtitles'}</td>
+                </tr>
+            </tbody>
+        </Table>
+    );
+};
