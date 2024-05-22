67cf5f30993793010f56cdc3d0845f1391a83501
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-22 18:17:43 +0300

    adjustments

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./user/userSlice";
 import toastSlice from "./toast/toastSlice";
 import cinemaSlice from "./cinema/cinemaSlice";
+import addMovieModalSlice from "./addMovieModal/addMovieModalSlice";
 
 export const store = configureStore({
     reducer: {
         user: userSlice,
         toast: toastSlice,
         cinema: cinemaSlice,
+        addMovieModal: addMovieModalSlice,
     },
 });
 
