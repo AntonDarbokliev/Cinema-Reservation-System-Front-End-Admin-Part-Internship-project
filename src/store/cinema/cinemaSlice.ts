22ba079729c138cfd0c6cef263701a89376d1231
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-22 18:16:39 +0300

    add cinema reducer

--- src/store/cinema/cinemaSlice.ts
+++ src/store/cinema/cinemaSlice.ts
@@ -1,5 +1,6 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { Cinema } from "../../features/CinemasList/interfaces/cinemaInterface";
+import { Movie } from "../../features/MoviesList/interfaces/Movie";
 
 const initialState: Cinema = {
     _id: "",
@@ -19,8 +20,11 @@ const cinemaSlice = createSlice({
         addCinema: (_: Cinema, action: PayloadAction<Cinema>) => {
             return action.payload;
         },
+        addMovieToCinema: (state: Cinema, action: PayloadAction<Movie>) => {
+            state.movies.push(action.payload)
+        }
     },
 });
 
-export const { addCinema } = cinemaSlice.actions;
+export const { addCinema, addMovieToCinema } = cinemaSlice.actions;
 export default cinemaSlice.reducer;
