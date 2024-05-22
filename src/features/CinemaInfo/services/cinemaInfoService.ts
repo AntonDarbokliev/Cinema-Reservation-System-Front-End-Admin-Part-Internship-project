5616f01a3b694a3e97b4f16f16f02c6561b8526a
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-22 13:29:53 +0300

    Remove unneeded components

--- src/features/CinemaInfo/services/cinemaInfoService.ts
+++
@@ -1,8 +0,0 @@
-import { Cinema } from "../../CinemasList/interfaces/cinemaInterface";
-import { RequestFactory } from "../../common/services/requester";
-
-const request = RequestFactory();
-const baseUrl = import.meta.env.VITE_API_BASE_URL;
-
-
-export const getCinema = (id: string): Promise<Cinema> => request.get(`${baseUrl}/cinemas/${id}`); 
