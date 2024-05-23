5f61f8c73201a0c365b7e7d58c0de75c70d48207
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-23 17:28:26 +0300

    adjustments

--- src/features/HallsList/hooks/useHalls.ts
+++ src/features/HallsList/hooks/useHalls.ts
@@ -8,7 +8,7 @@ export const useHalls = () => {
     const params = useParams();
     useEffect(() => {
         if (params.id) getHalls(params.id).then((data) => setHalls(data));
-    }, []);
+    }, [params.id]);
 
     return {
         halls,
