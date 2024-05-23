fadc2363ee909d3534f7706d11461ccdaccfb61e
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-23 22:03:04 +0300

    create food/beverage hook/service

--- src/features/FoodAndBeverages/service/foodAndBeveragesService.ts
+++ src/features/FoodAndBeverages/service/foodAndBeveragesService.ts
@@ -4,4 +4,8 @@ import { FoodAndBeverage } from "../interfaces/FoodAndBeverage";
 const request = RequestFactory();
 const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
-export const getAllFoodAndBeverages = async (cinemaId: string): Promise<FoodAndBeverage[]> => await request.get(`${baseUrl}/food-and-beverages/${cinemaId}`);
+export const getAllFoodAndBeverages = async (cinemaId: string): Promise<FoodAndBeverage[]> =>
+    await request.get(`${baseUrl}/food-and-beverages/${cinemaId}`);
+
+export const createFoodAndBeverage = async (foodAndBeverage: FormData): Promise<FoodAndBeverage> =>
+    await request.post(`${baseUrl}/food-and-beverages`, foodAndBeverage);
