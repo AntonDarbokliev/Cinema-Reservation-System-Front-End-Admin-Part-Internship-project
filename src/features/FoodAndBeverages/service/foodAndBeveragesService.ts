3bdf4fd389d7497402f1af279ab72400898e427d
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-23 19:55:42 +0300

    Food and beverages  service

---
+++ src/features/FoodAndBeverages/service/foodAndBeveragesService.ts
@@ -0,0 +1,7 @@
+import { RequestFactory } from "../../common/services/requester";
+import { FoodAndBeverage } from "../interfaces/FoodAndBeverage";
+
+const request = RequestFactory();
+const baseUrl = import.meta.env.VITE_API_BASE_URL;
+
+export const getAllFoodAndBeverages = async (cinemaId: string): Promise<FoodAndBeverage[]> => await request.get(`${baseUrl}/food-and-beverages/${cinemaId}`);
