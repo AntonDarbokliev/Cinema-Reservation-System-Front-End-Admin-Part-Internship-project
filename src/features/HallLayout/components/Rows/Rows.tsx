74a4a260a3f03d4b9d973ba066678f3bab3ad48a
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-26 19:56:12 +0300

    give seats status and give them one when clicking

--- src/features/HallLayout/components/Rows/Rows.tsx
+++ src/features/HallLayout/components/Rows/Rows.tsx
@@ -2,6 +2,9 @@ import { Button, Row as RowComponent } from "react-bootstrap";
 import { Row, Seat as SeatInterface, SeatType } from "../../../HallsList/interfaces/hallInterface";
 import { Seat } from "../Seat/Seat";
 import styles from "./Rows.module.scss";
+import { Reservation } from "../../../MovieDetails/interfaces/Reservation";
+import { SeatStatus } from "../../interfaces/SeatStatus";
+import { SelectedSeat } from "../../interfaces/SelectedSeat";
 
 interface Props {
     rows: Row[];
@@ -14,11 +17,29 @@ interface Props {
     >;
     editMode: boolean;
     addSeatType: SeatType;
+    reserveMode?: boolean;
+    reservations?: Reservation[];
+    setSelectedReserveSeat?: React.Dispatch<React.SetStateAction<SelectedSeat | null>>;
+    selectedReserveSeat?: SelectedSeat | null;
 }
 
-export const Rows: React.FC<Props> = ({ rows, editMode, addSeatType, setRows, deleteModalSetter }) => {
-    const seatOnClickHandler = (seat: SeatInterface, rowIndex: number) => {
-        if (editMode) {
+export const Rows: React.FC<Props> = ({
+    rows,
+    editMode,
+    addSeatType,
+    setRows,
+    deleteModalSetter,
+    reserveMode,
+    reservations,
+    setSelectedReserveSeat,
+    selectedReserveSeat,
+}) => {
+    const seatOnClickHandler = (seat: SeatInterface, rowIndex: number, seatNumber: number) => {
+        if (reserveMode && reservations && setSelectedReserveSeat) {
+            if (!reservations.some((reservation) => reservation.seat === seat._id)) {
+                setSelectedReserveSeat({ seat, seatRow: rowIndex + 1, seatNumber });
+            }
+        } else if (editMode) {
             if (seat.type === SeatType.SEAT_BLANK) {
                 seat.type = addSeatType;
             } else {
@@ -38,6 +59,7 @@ export const Rows: React.FC<Props> = ({ rows, editMode, addSeatType, setRows, de
         <>
             {rows.map((row, rowIndex) => {
                 currentSeatNumber = 0;
+                let seatStatus: SeatStatus | null;
                 return (
                     <RowComponent className={`${styles["row"]} ${editMode ? styles["row-edit"] : ""}`} key={row._id}>
                         {editMode && (
@@ -46,6 +68,18 @@ export const Rows: React.FC<Props> = ({ rows, editMode, addSeatType, setRows, de
                             </Button>
                         )}
                         {row.seats.map((seat) => {
+                            seatStatus = null;
+
+                            if (reservations) {
+                                if (reservations?.some((reservation) => reservation.seat === seat._id)) {
+                                    seatStatus = SeatStatus.SEAT_RESERVERED;
+                                } else if (selectedReserveSeat?.seat._id === seat._id) {
+                                    seatStatus = SeatStatus.SEAT_SELECTED;
+                                } else {
+                                    seatStatus = SeatStatus.SEAT_FREE;
+                                }
+                            }
+
                             if (seat.type != SeatType.SEAT_BLANK) {
                                 currentSeatNumber++;
                             }
@@ -58,6 +92,8 @@ export const Rows: React.FC<Props> = ({ rows, editMode, addSeatType, setRows, de
                                     seat={seat}
                                     seatNumber={currentSeatNumber}
                                     key={seat._id}
+                                    seatStatus={seatStatus}
+                                    isSelected={selectedReserveSeat?.seat._id === seat._id}
                                 />
                             );
                         })}
