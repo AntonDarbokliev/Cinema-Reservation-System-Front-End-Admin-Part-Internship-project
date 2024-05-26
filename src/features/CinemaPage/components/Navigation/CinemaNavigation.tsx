f4f8f5189cceadd0cafd537d005a48c94aa4bb38
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-26 20:04:35 +0300

    remove unnecessary navigation tabs

--- src/features/CinemaPage/components/Navigation/CinemaNavigation.tsx
+++ src/features/CinemaPage/components/Navigation/CinemaNavigation.tsx
@@ -5,11 +5,7 @@ import { Link } from "react-router-dom";
 export const CinemaNavigation = () => {
     return (
         <Nav justify variant="tabs" defaultActiveKey="/home" className={styles["nav"]}>
-            <Nav.Item className={styles["nav-item"]}>
-                <Nav.Link as={"div"} className={styles["nav-link"]} eventKey="link-0">
-                    <Link to="/">Info</Link>
-                </Nav.Link>
-            </Nav.Item>
+
             <Nav.Item className={styles["nav-item"]}>
                 <Nav.Link as={"div"} className={styles["nav-link"]} eventKey="link-1">
                     <Link to="halls">Halls</Link>
@@ -20,11 +16,7 @@ export const CinemaNavigation = () => {
                     <Link to="movies">Movies</Link>
                 </Nav.Link>
             </Nav.Item>
-            <Nav.Item className={styles["nav-item"]}>
-                <Nav.Link as={"div"} className={styles["nav-link"]} eventKey="link-3">
-                    <Link to="/">Projections</Link>
-                </Nav.Link>
-            </Nav.Item>
+
             <Nav.Item className={styles["nav-item"]}>
                 <Nav.Link as={"div"} className={styles["nav-link"]} eventKey="link-4">
                     <Link to="food-and-beverages">Food & Beverages</Link>
