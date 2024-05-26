036f15a0aa2074130ce66a9d3d468568c73d142a
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-26 19:51:42 +0300

    give projection a default value

--- src/features/ProjectionDetails/hooks/useProjection.ts
+++ src/features/ProjectionDetails/hooks/useProjection.ts
@@ -1,11 +1,36 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Projection } from "../../MovieDetails/interfaces/Projection";
+import { Projection, ProjectionType } from "../../MovieDetails/interfaces/Projection";
 import { getProjection } from "../service/ProjectionDetailsService";
+import { Rating } from "../../MoviesList/interfaces/Rating";
 
 export const useProjection = () => {
     const params = useParams();
-    const [projection, setProjection] = useState<Projection>();
+    const [projection, setProjection] = useState<Projection>({
+        _id: "",
+        baseTicketPrice: 12,
+        cinema: { _id: "", address: "", halls: [], menu: [], movies: [], name: "", numberOfHalls: "0", projections: [] },
+        hall: { _id: "", numberOfSeats: 0, seatsLayout: [], name: "" },
+        movie: {
+            _id: "",
+            actors: [],
+            director: "",
+            description: "",
+            genres: [],
+            language: "",
+            length: "",
+            name: "",
+            poster: "",
+            production: "",
+            rating: Rating.RATING_G,
+            subtitles: [],
+            projections: [],
+        },
+        projectionType: ProjectionType.PROJECTION_2D,
+        startDate: "",
+        startTime: "",
+        reservations: [],
+    });
     useEffect(() => {
         if (params.projectionId) {
             getProjection(params.projectionId).then((data) => setProjection(data));
@@ -13,5 +38,6 @@ export const useProjection = () => {
     }, []);
     return {
         projection,
+        setProjection,
     };
 };
