a10d107757ba47c6b94ef8b7424af5240e0bfedc
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-27 18:39:06 +0300

    projection services

--- src/features/ProjectionDetails/service/ProjectionDetailsService.ts
+++ src/features/ProjectionDetails/service/ProjectionDetailsService.ts
@@ -1,6 +1,9 @@
+import { CreateProjection } from "../../MovieDetails/interfaces/CreateProjection";
 import { RequestFactory } from "../../common/services/requester";
 
 const request = RequestFactory();
 const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
-export const getProjection = (id:string) => request.get(`${baseUrl}/projections/${id}`);
+export const getProjection = (id: string) => request.get(`${baseUrl}/projections/${id}`);
+export const getProjectionTypes = () => request.get(`${baseUrl}/projections/types`);
+export const createProjection = (projectionData: CreateProjection) => request.post(`${baseUrl}/projections`, projectionData);
