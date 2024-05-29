deda212e05babc07dd4c97d478b28045850819de
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-29 18:16:43 +0300

    details adjustments

--- src/features/MovieDetails/components/MovieDetailsProjection/MovieDetailsProjection.tsx
+++ src/features/MovieDetails/components/MovieDetailsProjection/MovieDetailsProjection.tsx
@@ -2,6 +2,7 @@ import { Card } from "react-bootstrap";
 import { UnpopulatedProjection } from "../../interfaces/unpopulatedProjection";
 import Button from "../../../common/components/Button/Button";
 import { useNavigate, useParams } from "react-router-dom";
+import styles from './MovieDetailsProjection.module.scss';
 
 interface Props {
     projection: UnpopulatedProjection;
@@ -11,7 +12,7 @@ export const MovieDetailsProjection: React.FC<Props> = ({ projection }) => {
     const navigate = useNavigate();
     const params = useParams();
     return (
-        <Card style={{ width: "18rem" }}>
+        <Card className={styles["projection-card"]} style={{ width: "18rem" }}>
             <Card.Body>
                 <Card.Title>{projection.projectionType}</Card.Title>
                 <Card.Subtitle className="mb-2 text-muted">{projection.startDate}</Card.Subtitle>
--- src/features/MovieDetails/components/MovieDetailsProjections/MovieDetailsProjections.tsx
+++ src/features/MovieDetails/components/MovieDetailsProjections/MovieDetailsProjections.tsx
@@ -3,17 +3,24 @@ import { IRootState } from "../../../../store/store";
 import { useParams } from "react-router-dom";
 import { MovieDetailsProjection } from "../MovieDetailsProjection/MovieDetailsProjection";
 import Button from "../../../common/components/Button/Button";
+import styles from './MovieDetailsProjections.module.scss';
+import { useState } from "react";
+import { AddProjectionModal } from "../AddProjectionModal/AddProjectionModal";
 
 export const MovieDetailsProjections = () => {
     const params = useParams();
     const cinema = useSelector((state: IRootState) => state.cinema);
+    const [showAddProjection, setShowAddProjectionModal] = useState(false);
     const projections = cinema?.projections.filter((prj) => prj.movie == params.movieId);
     return (
         <>
             <h1>
-                Projections <Button>Add a Projection</Button>
+                Projections <Button onClick={() => setShowAddProjectionModal(true)}>Add a Projection</Button>
             </h1>
+            <AddProjectionModal show={showAddProjection} showAddProjectionModal={setShowAddProjectionModal}></AddProjectionModal>
+            <div className={styles["projections-list"]}>
             {projections && projections.length > 0 && projections.map((prj) => <MovieDetailsProjection key={prj._id} projection={prj} />)}
+            </div>
             {!projections ||
                 (projections.length == 0 && (
                     <>
