697f9ae18436e13e211cd8c1e9388b09dbff8a95
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-29 18:17:03 +0300

    retreive halls from a new endpoint

--- src/features/HallsList/service/hallsListService.ts
+++ src/features/HallsList/service/hallsListService.ts
@@ -1,4 +1,3 @@
-import { Cinema } from "../../CinemasList/interfaces/cinemaInterface";
 import { RequestFactory } from "../../common/services/requester";
 import { CreateHall, Hall } from "../interfaces/hallInterface";
 
@@ -6,8 +5,7 @@ const request = RequestFactory();
 const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
 export const getHalls = async (cinemaId: string): Promise<Hall[]> => {
-    const cinema: Cinema = await request.get(`${baseUrl}/cinemas/${cinemaId}`);
-    return cinema.halls;
-};
+    return await request.get(`${baseUrl}/halls/cinema/${cinemaId}`);
+}
 
 export const createHall = (hallData: CreateHall): Promise<Hall> => request.post(`${baseUrl}/halls`, hallData);
