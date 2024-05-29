4b31682a0520e53daf501fa4fe76216129c3d48b
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-29 18:19:20 +0300

    hall services

--- src/features/HallLayout/service/hallLayoutService.ts
+++ src/features/HallLayout/service/hallLayoutService.ts
@@ -1,3 +1,4 @@
+import { SeatType } from "../../HallsList/interfaces/SeatType";
 import { CreateRow, Hall } from "../../HallsList/interfaces/hallInterface";
 import { RequestFactory } from "../../common/services/requester";
 import { CreateReservation } from "../interfaces/CreateReservation";
@@ -8,3 +9,5 @@ const baseUrl = import.meta.env.VITE_API_BASE_URL;
 export const getHall = (id: string): Promise<Hall> => request.get(`${baseUrl}/halls/${id}`);
 export const editHall = (id: string, layout: { seatsLayout: CreateRow[] }): Promise<Hall> => request.patch(`${baseUrl}/halls/${id}`, layout);
 export const reserveSeat = (data: CreateReservation) => request.post(`${baseUrl}/reservations`, data);
+export const getFoodAndBeverages = (cinemaId: string) => request.get(`${baseUrl}/food-and-beverages/${cinemaId}`);
+export const getSeatTypes = (cinemaId: string): Promise<SeatType[]> => request.get(`${baseUrl}/seat-types/${cinemaId}`);
