02f2b2d55a2ea43171e99fa98cb52447480c8969
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-29 18:15:26 +0300

    add projection action

--- src/store/cinema/cinemaSlice.ts
+++ src/store/cinema/cinemaSlice.ts
@@ -1,6 +1,7 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { Cinema } from "../../features/CinemasList/interfaces/cinemaInterface";
 import { Movie } from "../../features/MoviesList/interfaces/Movie";
+import { UnpopulatedProjection } from "../../features/MovieDetails/interfaces/unpopulatedProjection";
 
 const initialState: Cinema = {
     _id: "",
@@ -21,10 +22,13 @@ const cinemaSlice = createSlice({
             return action.payload;
         },
         addMovieToCinema: (state: Cinema, action: PayloadAction<Movie>) => {
-            state.movies.push(action.payload)
-        }
+            state.movies.push(action.payload);
+        },
+        addProjectionToCinema: (state: Cinema, action: PayloadAction<UnpopulatedProjection>) => {
+            state.projections.push(action.payload);
+        },
     },
 });
 
-export const { addCinema, addMovieToCinema } = cinemaSlice.actions;
+export const { addCinema, addMovieToCinema, addProjectionToCinema } = cinemaSlice.actions;
 export default cinemaSlice.reducer;
