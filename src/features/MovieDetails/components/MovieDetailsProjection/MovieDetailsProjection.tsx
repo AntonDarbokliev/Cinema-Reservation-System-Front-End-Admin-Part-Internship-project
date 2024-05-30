155ecc5fe2fdc0962edecf1f9417369164c387bb
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-30 15:34:56 +0300

    Dispaly a movie's status

--- src/features/MovieDetails/components/MovieDetailsProjection/MovieDetailsProjection.tsx
+++ src/features/MovieDetails/components/MovieDetailsProjection/MovieDetailsProjection.tsx
@@ -1,19 +1,36 @@
-import { Card } from "react-bootstrap";
-import { UnpopulatedProjection } from "../../interfaces/unpopulatedProjection";
+import Card from "react-bootstrap/Card";
+import Badge from "react-bootstrap/Badge";
 import Button from "../../../common/components/Button/Button";
 import { useNavigate, useParams } from "react-router-dom";
-import styles from './MovieDetailsProjection.module.scss';
+import styles from "./MovieDetailsProjection.module.scss";
+import { ProjectionStatus } from "../../interfaces/ProjectionStatus";
+import { Projection } from "../../interfaces/Projection";
 
 interface Props {
-    projection: UnpopulatedProjection;
+    projection: Projection;
 }
 
 export const MovieDetailsProjection: React.FC<Props> = ({ projection }) => {
     const navigate = useNavigate();
     const params = useParams();
+    const pillColor = () => {
+        switch (projection.status) {
+            case ProjectionStatus.PROJECTION_SCHEDULED:
+                return "primary";
+            case ProjectionStatus.PROJECTION_AWAITING:
+                return "warning";
+            case ProjectionStatus.PROJECTION_RUNNING:
+                return "success";
+            case ProjectionStatus.PROJECTION_ENDED:
+                return "danger";
+        }
+    };
     return (
         <Card className={styles["projection-card"]} style={{ width: "18rem" }}>
             <Card.Body>
+                <Badge pill bg={pillColor()}>
+                    {projection.status}
+                </Badge>
                 <Card.Title>{projection.projectionType}</Card.Title>
                 <Card.Subtitle className="mb-2 text-muted">{projection.startDate}</Card.Subtitle>
                 <Card.Text>{projection.startTime}</Card.Text>
