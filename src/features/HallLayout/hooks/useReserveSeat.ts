e8a8e3d1c8a26d647b962bb9f9aac18bad5d74e5
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-06-04 17:19:31 +0300

    dispatch socket action if connected

--- src/features/HallLayout/hooks/useReserveSeat.ts
+++ src/features/HallLayout/hooks/useReserveSeat.ts
@@ -1,9 +1,19 @@
 import { reserveSeat } from "../service/hallLayoutService";
 import { CreateReservation } from "../interfaces/CreateReservation";
+import { useDispatch, useSelector } from "react-redux";
+import { IRootState } from "../../../store/store";
+import { reserveSeat as reserveSeatAction } from "../../../store/webSocket/socketSlice";
 
 export const useReserveSeat = () => {
+    const isConnected = useSelector((state: IRootState) => state.socket.isConnected);
+    const dispatch = useDispatch();
+
     const reserveSeatHandler = async (reserveData: CreateReservation) => {
-        return await reserveSeat(reserveData);
+        const reservation = await reserveSeat(reserveData);
+        if (isConnected) {
+            dispatch(reserveSeatAction({ reservation }));
+        }
+        return reservation;
     };
 
     return {
