4ec53220570ecda3b547ce25c8eb0e8fada1a223
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-06-04 15:15:51 +0300

    add middleware/slice to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -6,6 +6,8 @@ import addCinemaModalSlice from "./addCinemaModal/addCinemaModalSlice";
 import addHallModalSlice from "./addHallModal/addHallModalSlice";
 import addFoodAndBeverageModalSlice from "./addFoodBeverageModal/addFoodBeverageModalSlice";
 import addEditMovieModalSlice from "./addEditMovieModal/addEditMovieModalSlice";
+import socketMiddleware from "./webSocket/socketMiddleware";
+import socketSlice from "./webSocket/socketSlice";
 
 export const store = configureStore({
     reducer: {
@@ -16,6 +18,10 @@ export const store = configureStore({
         addCinemaModal: addCinemaModalSlice,
         addHallModal: addHallModalSlice,
         addFoodAndBeverageModal: addFoodAndBeverageModalSlice,
+        socket: socketSlice,
+    },
+    middleware(getDefaultMiddleware) {
+        return getDefaultMiddleware().concat([socketMiddleware]);
     },
 });
 
