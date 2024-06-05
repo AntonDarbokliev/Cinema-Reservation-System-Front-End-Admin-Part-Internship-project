7b24bd23e75748911a5c08fa962f486a03d01636
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-06-05 14:56:45 +0300

    new event action

--- src/store/webSocket/socketSlice.ts
+++ src/store/webSocket/socketSlice.ts
@@ -21,6 +21,8 @@ const initialState: SocketState = {
     tickets: [],
 };
 
+type SharedStateAction = PayloadAction<SocketState>;
+
 type SeatSelectAction = PayloadAction<{
     seat: SocketSeat | null;
     fromServer?: boolean;
@@ -28,6 +30,11 @@ type SeatSelectAction = PayloadAction<{
 
 type SeatReserveAction = PayloadAction<{
     reservation: Reservation;
+    fromServer?: boolean;
+}>;
+
+type SeatUnreserveAction = PayloadAction<{
+    id: string;
     fromServer?: boolean;
 }>;
 
@@ -44,8 +51,12 @@ const socketSlice = createSlice({
             // A function made just to trigger the middleware, thus initializing the socket.
             return;
         },
-        connectionEstablished: (state) => {
+        connectionEstablished: (state, action: SharedStateAction) => {
             state.isConnected = true;
+
+            state.seats = action.payload.seats;
+            state.reservations = action.payload.reservations;
+            state.tickets = action.payload.tickets;
         },
         connectionLost: (state) => {
             state.isConnected = false;
@@ -58,9 +69,17 @@ const socketSlice = createSlice({
             if (action.payload.seat) state.seats = state.seats.filter((seat) => seat._id !== action.payload.seat!._id);
         },
         reserveSeat: (state, action: SeatReserveAction) => {
-            if (action.payload.reservation) {
+            const doesExist = state.reservations.some((reservation) => reservation._id === action.payload.reservation._id);
+            if (action.payload.reservation && !doesExist) {
                 state.reservations.push(action.payload.reservation);
                 state.seats.filter((seat) => seat._id !== action.payload.reservation.seat);
+            }
+        },
+        unreserveSeat: (state, action: SeatUnreserveAction) => {
+            if (action.payload.id) {
+                console.log("Unreserving seat: ", action.payload.id);
+
+                state.reservations = state.reservations.filter((res) => res._id !== action.payload.id);
             }
         },
         buySeat: (state, action: SeatBuyAction) => {
@@ -72,6 +91,7 @@ const socketSlice = createSlice({
     },
 });
 
-export const { connectionEstablished, connectionLost, initSocket, selectSeat, unselectSeat, buySeat, reserveSeat, } = socketSlice.actions;
+export const { connectionEstablished, connectionLost, initSocket, selectSeat, unselectSeat, buySeat, reserveSeat, unreserveSeat } =
+    socketSlice.actions;
 
 export default socketSlice.reducer;
