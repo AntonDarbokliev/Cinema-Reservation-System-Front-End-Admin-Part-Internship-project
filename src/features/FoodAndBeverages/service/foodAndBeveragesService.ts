a78522cff4aed7ee79bbc5d1d90af1ac96d59ef4
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-06-08 18:40:06 +0300

    delete modal/hook/service

--- src/features/FoodAndBeverages/service/foodAndBeveragesService.ts
+++ src/features/FoodAndBeverages/service/foodAndBeveragesService.ts
@@ -12,3 +12,6 @@ export const createFoodAndBeverage = async (foodAndBeverage: FormData): Promise<
 
 export const editFoodAndBeverage = async (foodAndBeverage: FormData, foodAndBeverageId: string): Promise<FoodAndBeverage> =>
     await request.patch(`${baseUrl}/food-and-beverages/${foodAndBeverageId}`, foodAndBeverage);
+
+export const deleteFoodAndBeverage = async (foodAndBeverageId: string): Promise<FoodAndBeverage> =>
+    await request.delete(`${baseUrl}/food-and-beverages/${foodAndBeverageId}`);
