e0fc7be9b7686fffeb06a59eeaf07318a7600b3d
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-06-08 18:12:56 +0300

    suit component to work with editing food and beverages

--- src/features/FoodAndBeverages/components/FoodAndBeveragesList/FoodAndBeveragesList.tsx
+++ src/features/FoodAndBeverages/components/FoodAndBeveragesList/FoodAndBeveragesList.tsx
@@ -3,18 +3,26 @@ import { useFoodAndBeverages } from "../../hooks/useFoodAndBeverages";
 import { FoodAndBeverageCard } from "../FoodAndBeverageCard/FoodAndBeverageCard";
 import { IRootState } from "../../../../store/store";
 import { useSelector } from "react-redux";
-import { AddFoodAndBeverageModal } from "../AddFoodAndBeverageModal/AddFoodAndBeverageModal";
+import { AddEditFoodAndBeverageModal } from "../AddEditFoodAndBeverageModal/AddEditFoodAndBeverageModal";
 import styles from "./FoodAndBeveragesList.module.scss";
+import { FoodAndBeverage } from "../../interfaces/FoodAndBeverage";
+import { useState } from "react";
 
 export const FoodAndBeveragesList = () => {
     const { foodBeverages, setFoodBeverages } = useFoodAndBeverages();
+    const [foodAndBeverageToEdit, setFoodAndBeverageToEdit] = useState<FoodAndBeverage | null>(null);
     const show = useSelector((state: IRootState) => state.addFoodAndBeverageModal.show);
     return (
         <Container>
-            <AddFoodAndBeverageModal stateSetter={setFoodBeverages} show={show} />
+            <AddEditFoodAndBeverageModal
+                setFoodAndBeverage={setFoodAndBeverageToEdit}
+                foodAndBeverage={foodAndBeverageToEdit ?? null}
+                stateSetter={setFoodBeverages}
+                show={show}
+            />
             <div className={styles["food-and-beverages-list"]}>
                 {foodBeverages.map((f) => (
-                    <FoodAndBeverageCard key={f._id} foodAndBeverage={f} />
+                    <FoodAndBeverageCard setFoodAndBeverageToEdit={setFoodAndBeverageToEdit} key={f._id} foodAndBeverage={f} />
                 ))}
             </div>
         </Container>
