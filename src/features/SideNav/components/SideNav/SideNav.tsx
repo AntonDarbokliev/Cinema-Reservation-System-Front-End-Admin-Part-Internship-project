9a2946c899295784bc346605e134be751cc15eab
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-06-09 13:40:46 +0300

    Settings modal btn

--- src/features/SideNav/components/SideNav/SideNav.tsx
+++ src/features/SideNav/components/SideNav/SideNav.tsx
@@ -10,12 +10,14 @@ import { showAddEditMovieModal } from "../../../../store/addEditMovieModal/addEd
 import { showAddCinemaModal } from "../../../../store/addCinemaModal/addCinemaModalSlice";
 import { showAddHallModal } from "../../../../store/addHallModal/addHallModalSlice";
 import { showAddFoodAndBeverageModal } from "../../../../store/addFoodBeverageModal/addFoodBeverageModalSlice";
+import { SettingsModal } from "../SettingsModal/SettingsModal";
 
 export const SideNav = () => {
-    const [show, setShow] = useState(false);
+    const [showOffCanvas, setShowOffCanvas] = useState(false);
+    const [showSettingsModal, setShowSettingsModal] = useState(false);
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+    const handleClose = () => setShowOffCanvas(false);
+    const handleShow = () => setShowOffCanvas(true);
 
     const linkRefs = useRef<HTMLAnchorElement[]>([]);
 
@@ -35,7 +37,7 @@ export const SideNav = () => {
 
     return (
         <>
-            <Offcanvas show={show} onHide={handleClose} placement="end">
+            <Offcanvas show={showOffCanvas} onHide={handleClose} placement="end">
                 <Offcanvas.Header closeButton>
                     <Offcanvas.Title>Menu {user.firstName ? `- ${user.firstName} ${user.lastName}` : ""}</Offcanvas.Title>
                 </Offcanvas.Header>
@@ -70,25 +72,43 @@ export const SideNav = () => {
                         )}
 
                         {user.email && (
-                            <Nav.Item onClick={() => handleTabClick(3)}>
-                                <Nav.Link eventKey="third">
-                                    <Link ref={(el) => (el != null ? (linkRefs.current[3] = el) : {})} to="/logout">
-                                        Logout
-                                    </Link>
-                                </Nav.Link>
-                            </Nav.Item>
+                            <>
+                                <Nav.Item onClick={() => handleTabClick(3)}>
+                                    <Nav.Link eventKey="third">
+                                        <Link
+                                            ref={(el) => (el != null ? (linkRefs.current[3] = el) : {})}
+                                            to="#"
+                                            onClick={() => setShowSettingsModal(true)}
+                                        >
+                                            Settings
+                                        </Link>
+                                    </Nav.Link>
+                                </Nav.Item>
+
+                                <Nav.Item onClick={() => handleTabClick(4)}>
+                                    <Nav.Link eventKey="third">
+                                        <Link ref={(el) => (el != null ? (linkRefs.current[4] = el) : {})} to="/logout">
+                                            Logout
+                                        </Link>
+                                    </Nav.Link>
+                                </Nav.Item>
+                            </>
                         )}
                     </Nav>
                 </Offcanvas.Body>
             </Offcanvas>
+            <SettingsModal show={showSettingsModal} setShow={setShowSettingsModal} />
+
             <div className={styles["side-buttons-div"]}>
                 <Button onClick={handleShow} additionalClasses={`me-2 ${styles["side-nav-button"]}`}>
                     Menu
                 </Button>
                 {cinemaMoviesPathPattern.test(location.pathname) && <Button onClick={() => dispatch(showAddEditMovieModal())}>Add Movie</Button>}
                 {cinemaHallsPathPattern.test(location.pathname) && <Button onClick={() => dispatch(showAddHallModal())}>Add Hall</Button>}
-                {cinemaFoodAndBeveragesPathPattern.test(location.pathname) && <Button onClick={() => dispatch(showAddFoodAndBeverageModal())}>Add Item</Button>}
-                {location.pathname === '/' && <Button onClick={() => dispatch(showAddCinemaModal())}>Add Cinema</Button>}
+                {cinemaFoodAndBeveragesPathPattern.test(location.pathname) && (
+                    <Button onClick={() => dispatch(showAddFoodAndBeverageModal())}>Add Item</Button>
+                )}
+                {location.pathname === "/" && <Button onClick={() => dispatch(showAddCinemaModal())}>Add Cinema</Button>}
             </div>
         </>
     );
