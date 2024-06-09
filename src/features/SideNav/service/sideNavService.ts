08818741c09d567dc54246b195a1bcdaf1e8c1ba
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-06-09 23:37:31 +0300

    edit seat type hook/service

--- src/features/SideNav/service/sideNavService.ts
+++ src/features/SideNav/service/sideNavService.ts
@@ -4,3 +4,5 @@ const request = RequestFactory();
 const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
 export const addSeatType = (data: FormData) => request.post(`${baseUrl}/seat-types`, data);
+export const editSeatType = (data: FormData, seatTypeId: string) => request.patch(`${baseUrl}/seat-types/${seatTypeId}`, data);
+export const deleteSeatType = (seatTypeId: string) => request.delete(`${baseUrl}/seat-types/${seatTypeId}`);
